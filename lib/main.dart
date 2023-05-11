07cf46b28bd0c7913dcf9ae58b5a0e3285f4da52
Appal <appala@easycloud.in>
2023-05-11 19:23:20 +0530

    chore: deploy site on firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:json_to_dart/app.dart';
+import 'package:json_to_dart/firebase_options.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const App());
 }
